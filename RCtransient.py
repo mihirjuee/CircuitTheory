4ee1caed33c49f5cc6fad361dd9c4c7951d6cdae
mihirjuee <mihir.juee@gmail.com>
2026-04-20 12:55:52 +0530

    Update RCtransient.py

--- RCtransient.py
+++ RCtransient.py
@@ -33,6 +33,9 @@ def draw_circuit(active_path="charge"):
     # Switch
     if active_path == "charge":
         sw = elm.Switch(action='close').right()
+        d += elm.Line().down(0.2)
+        d += elm.Switch(action='close').down()
+        d += elm.Line().down(1)
     else:
         sw = elm.Switch(action='open').right()
 
