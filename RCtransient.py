c7bd2b041f5a86f92aa3230a647189245feee871
mihirjuee <mihir.juee@gmail.com>
2026-04-20 10:29:38 +0530

    Update RCtransient.py

--- RCtransient.py
+++ RCtransient.py
@@ -42,7 +42,7 @@ def draw_circuit(active_path="charge"):
 
     # Return path
     d += elm.Line().left(6)
-    d += elm.Line().up(2.7)
+    d += elm.Line().up(2.8)
 
     # Discharge loop
     if active_path == "discharge":
