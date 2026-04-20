5cfceff02a95a7269a915d728d4105a5d0bea640
mihirjuee <mihir.juee@gmail.com>
2026-04-20 10:47:43 +0530

    Update RCtransient.py

--- RCtransient.py
+++ RCtransient.py
@@ -55,7 +55,7 @@ def draw_circuit(active_path="charge"):
         d.push()
         d += elm.Line().down(1)
         d += elm.Switch(action='close')
-        d += elm.Line().down(2)        
+        d += elm.Line().down(1.5)        
         d.pop()
 
     return d
